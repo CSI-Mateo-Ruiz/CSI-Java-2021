48743ea2b6b78c011f2f29e584cd685914479037
CSI-Mateo-Ruiz <89141125+CSI-Mateo-Ruiz@users.noreply.github.com>
2021-08-20 09:07:31 -0400

    Classwork
    Trabajo de clase hello world

---
+++ Modules/Module2/src/csi/ruiz/pkg/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package csi.ruiz.pkg;
+
+/**
+ * @author Mateo
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+		System.out.println("My name is Mateo Ruiz - Montilla");
+		System.out.println("Student number 24-088");
+	}
+	
+	
+
+}
