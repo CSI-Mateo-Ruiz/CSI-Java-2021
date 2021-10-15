db9e9d4c598a0aecc32540613c6a42d2ef12047a
CSI-Mateo-Ruiz <89141125+CSI-Mateo-Ruiz@users.noreply.github.com>
2021-10-15 11:11:37 -0400

    mira si te llega

--- MainProject/csi/ruiz/pkg/laX.java
+++ MainProject/csi/ruiz/pkg/laX.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class laX {
 	public static void main(String[] args) {
-//		int[][] arr = new int[9][9];
-//		for (int i = 0; i < arr.length; i++) {
-//			arr[i][i] = 1;
-//			arr[i][arr[i].length - 1 - i] = 1;
-//
-//			System.out.println(Arrays.toString(arr[i]));
-//		}
-		// Invert(arr);
+		int[][] arr = new int[9][9];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i][i] = 1;
+			arr[i][arr[i].length - 1 - i] = 1;
+
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		 Invert(arr);
 				int[][][] arr1 = new int[2][9][9];
 				System.out.println("New Method");
 				System.out.println("--------------------------------");
@@ -29,12 +29,12 @@ public class laX {
 				System.out.println("--------------------------------");
 				for (int j = 0; j < arr1[1].length; j++) {
 					for (int k = 0; k < arr1[1][j].length; k++) {
-//						arr1[1][j][k] = 1 - arr1[0][j][k];
-						if(arr1[0][j][k]==1) {
-							arr1[1][j][k]=0;
-						}else {
-							arr1[1][j][k]=1;
-						}
+						arr1[1][j][k] = 1 - arr1[0][j][k];
+//						if(arr1[0][j][k]==1) {
+//							arr1[1][j][k]=0;
+//						}else {
+//							arr1[1][j][k]=1;
+//						}
 					}
 					System.out.println(Arrays.toString(arr1[1][j]));
 				}
@@ -42,13 +42,7 @@ public class laX {
 
 	
 	
-	
-	
-	
-	
-	
-	
-	
+		
 	public static void Invert(int[][] arr) {
 		System.out.println("----------------Second Method---------------");
 		System.out.println("---------------------------");
