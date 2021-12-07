3cd1daa9c5b37468cd0fa12202d16f55956f9752
CSI-Mateo-Ruiz <89141125+CSI-Mateo-Ruiz@users.noreply.github.com>
2021-12-07 09:25:09 -0400

    no message

--- MainProject/csi/mateo/finance/pkg/Calculator.java
+++ MainProject/csi/mateo/finance/pkg/Calculator.java
@@ -4,48 +4,56 @@ import java.util.*;
 
 public class Calculator {
 	static String vehicle = "none";
-
+	static String job;
+	static String house;
+	
 	public static void main(String[] args) {
 		double banco = 0;
-		
-	
+
 		double invested = 0;
 		double paycheck = 0;
 		String location = "none";
-		
-		//==========loans=============\\
+
+		// ==========loans=============\\
 		List<Loan> loans = new ArrayList<Loan>();
-		loans.add(new Loan("Student Loans", 0, 0.0466, 0, 0));
-		loans.add(new Loan("Personal Loans", 0, 0.16, 0, 0));
-		loans.add(new Loan("Mortgage Loans", 0, 0.03125, 0, 0));
-		loans.add(new Loan("CreditCard", 0, 0.29, 0, 0));
-		
-		for (int age = 0; age <= 80; age++) {
+		loans.add(new Loan("Student Loans", 0, 0.0466, 10));
+		loans.add(new Loan("Personal Loans", 0, 0.16, 10));
+		loans.add(new Loan("Mortgage Loans", 0, 0.03125, 15));
+		loans.add(new Loan("CreditCard", 0, 0.29, 99));
+
+		for (int age = 0; age <= 90; age++) {
 			if (age <= 15) {
 				location = "Puerto Rico";
 				double salary = 0;
 				banco += salary;
 				vehicle = "none";
+				job = "unemployed";
+				loans.get(0).differed = true;
 
 			} else if (age <= 17) {
 				paycheck = 750;
 				banco += paycheck;
 				vehicle = "Honda Pilot";
 				location = "Puerto Rico";
+				job = "unemployed";
+				loans.get(0).differed = true;
 
 			} else if (age <= 21) {
 				// pre med
 				location = "San Francisco";
 				double salary = 20_000;
 				double tuition = 0;
-				double costOfLiving = (12 * 420) + (12 * 580);
+				double costOfLiving = 12 * 1_773 + 6_960;
 				vehicle = "bike";
+
 				// papa me regala una bici
 
 				banco += salary;
-				banco -= 15_000;
-				loans.get(0).balance += tuition + costOfLiving;
-				loans.get(0).balance -= 15_000;
+				banco -= 12_000;
+				loans.get(0).principal += tuition + costOfLiving;
+				loans.get(0).principal -= 12_000;
+				job = "Part time fast food employee";
+				loans.get(0).differed = true;
 
 			} else if (age <= 25) {
 
@@ -53,39 +61,165 @@ public class Calculator {
 				location = "San Francisco";
 				double salary = 20_000;
 				double tuition = 53_529;
-				double costOfLiving = 22_200 + 4_105.32;
+				double costOfLiving = 12 * 2_170 + 6_960;
 
-//44,400 + 4,105.32 estoy viviendo con un roommate
-
-				loans.get(0).balance += tuition + costOfLiving;
+				loans.get(0).principal += tuition + costOfLiving;
 				banco += salary;
+				job = "Part time fast food employee";
+				loans.get(0).differed = true;
 
 			} else if (age == 26) {
 				// internship general surgery
-				double costOfLiving = (12 * 420) + (12 * 580);
+				double costOfLiving = 12 * 1_780;
 				location = "Puerto Rico";
-				loans.get(0).balance += costOfLiving;
+				loans.get(0).principal += costOfLiving;
 				// 420 = comida y 580 es renta
+				job = "an intern at general surgery";
+				loans.get(0).differed = true;
+
+			} else if (age == 27) {
+				double salary = 60_000;
+				double buyVehicle = 15_000;
+				vehicle = "2016 toyota corola";
+
+				banco += salary;
+				banco -= buyVehicle;
+				job = "Resident";
+				
+				loans.get(0).differed = false;
+				
+			} else if (age == 28) {
+				double salary = 60_000;
+
+				vehicle = "2016 toyota corola";
+
+				banco += salary;
+				job = "Resident";
+				
+
+			} else if (age == 29) {
+				double salary = 60_000;
+				
+				// casa = 275_000
+				
+				 double apartmentPrice = 275_000;
+			        double downPayment = apartmentPrice * 0.15;
+			        double interestRate = 0.03;
+			        int repaymentYears = 15;
+				
+			        Loan mortgageLoan = new Loan("My first home", 
+			                (apartmentPrice - downPayment),
+			                interestRate, 
+			                repaymentYears);
+			        loans.add( mortgageLoan );
+			        
+			        System.out.println("House mortgage: " + mortgageLoan.getMonthlyPayment() + " For a total of " + mortgageLoan.getTotalPayment() );
+
+				vehicle = "2016 toyota corola";
+				job = "Resident";
+				house = "house in parkville";
+				
+
+				banco += salary;
+				banco -= 2_000;
+				banco -= downPayment;
+				// invest 2_000\\
+				
+				
+
+			} else if (age == 30) {
+				double salary = 60_000;
+				double childDelivery = 3_068;
+
+				vehicle = "2014 toyota corola";
+
+				banco += salary;
+				banco -= childDelivery;
+				job = "Resident";
+				
 
 			} else if (age <= 33) {
 //	$10,339 to $184,950 average residency rate in Puerto rico and US
 				double salary = 60_000;
+				location = "Puerto Rico";
 				banco += salary;
-				loans.get(0).balance -= 33_000;
-				banco -= 35_000;
-//				quiero invest 2000
+				job = "Resident";
+				
+
+			
+				banco -= 3_000;
+//				quiero invest 3000
 //						5 years' experience: $301,000
 //						10 years: $397,000
 //						20+ years: $414,000.
+
+			} else if (age <= 38) {
+				double salary = 301_000;
+				location = "Puerto Rico";
+				banco += salary;
+
+				loans.get(0).makePayment();
+				loans.get(3).makePayment();
+
+				banco -= 10_000;
+				banco -= loans.get(0).makePayment();
+				banco -= loans.get(3).makePayment();
+				// invest 10000
+				job = "Neurosurgeon";
+				
+
+			} else if (age == 39) {
+				double salary = 397_000;
+				banco += salary;
+				
+
+				banco -= 10_000;
+				// invest 10000
+
+			} else if (age <= 43) {
+				double salary = 397_000;
+				location = "Puerto Rico";
+				banco += salary;
+				
+
+				banco -= 10_000;
+				// invest 10000
+
+			} else if (age <= 48) {
+				double salary = 414_000;
+				location = "Puerto Rico";
+				banco += salary;
+				
+
+				banco -= 10_000;
+				// invest 10000
+
+			} else if (age <= 50) {
+				double salary = 414_000;
+				location = "Puerto Rico";
+				banco += salary;
+
+			} else if (age <= 60) {
+					job = "retired";
 			}
 			
+			///////////// monthly Loan payments\\\\\\\\
+			for (Loan loan : loans) {
+				for (int month = 0; month < 12; month++) {
+					if (loan.getBalance() > 0 && loan.differed == false) {
+						banco -= loan.makePayment();
+					}
+				}
+			}
+			
+			
+			
 			double debt = 0;
-			for(Loan loan : loans)
-			  debt += loan.balance;
-
+			for (Loan loan : loans)
+				debt += loan.getBalance();
 			System.out.println("He is living in " + location + ". Balance at age: " + age + " is: " + banco
-					+ " with a debt of " + debt + " and " + invested + " invested. He is driving a " + vehicle);
+					+ " with a debt of " + debt + " and " + invested + " invested. He is driving a " + vehicle
+					+ " and is working as a " + job + "." + " He is livving in " + house);
 		}
 	}
-
 }
--- MainProject/csi/mateo/finance/pkg/Loan.java
+++ MainProject/csi/mateo/finance/pkg/Loan.java
@@ -1,39 +1,49 @@
 package csi.mateo.finance.pkg;
 
 public class Loan {
+	
 	String name;
-	double balance;
-	double rate;
-	double paymentAmount;
-	int startAge;
-
-	public Loan(String name, double balance, double rate, double paymentAmount, int startAge) {
+	double principal;
+	double annualInterestRate;
+	double numberOfYears;
+	
+	double paid;
+	boolean differed = false;
+	
+	public Loan(String name, double principal, double annualInterestRate, int numberOfYears) {
 		super();
 		this.name = name;
-		this.balance = balance;
-		this.rate = rate;
-		this.paymentAmount = paymentAmount;
-		this.startAge = startAge;
-
+		this.principal = principal;
+		this.annualInterestRate = annualInterestRate;
+		this.numberOfYears = numberOfYears;
+			
 	}
-
+	
 	public double makePayment() {
-
-		balance -= paymentAmount;
-		if (balance < 0) {
-			double overpay = paymentAmount + balance;
-			balance = 0;
-			return overpay;
-
+		
+		if(getBalance() < getMonthlyPayment()) {
+			double due = getBalance();
+			paid += due;
+			return due;
+	
+		} else {
+			paid += getMonthlyPayment();
+			return getMonthlyPayment();
 		}
-
-		return paymentAmount;
-
 	}
-
-	public void accurateInterest(int frequency) {
-
-		balance += balance * rate;
+	
+	public double getBalance() {
+		return (getTotalPayment() - paid);
+	}
+	
+	public double getMonthlyPayment() {
+		double monthlyInterestRate = annualInterestRate / 1200;
+		double monthlyPayment = principal * monthlyInterestRate / (1 -1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+		return monthlyPayment;
+	}
+	
+	public double getTotalPayment() {
+		return (getMonthlyPayment() * numberOfYears * 12);
 	}
 
 }
