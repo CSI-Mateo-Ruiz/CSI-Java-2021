14e3b9feff4b47358b1e37df624da78777c2eb3e
CSI-Mateo-Ruiz <89141125+CSI-Mateo-Ruiz@users.noreply.github.com>
2022-02-11 09:30:24 -0400

    el negrito de ojos claro

--- MainProject/src/csi/ruiz/inheritance/DogPound.java
+++ MainProject/src/csi/ruiz/inheritance/DogPound.java
@@ -1,14 +1,16 @@
 package csi.ruiz.inheritance;
 
-import java.util.*;
+import javax.swing.JPanel;
 
-public class DogPound {
+
+public class DogPound extends JPanel {
 	static Dog[] dogPound;
 
 	public static void main(String[] args) {
 		
 		dogPound = new Dog[10];
-		dogPound[0] = new Dog("chihuawa","white", 4 , false, false);
+		dogPound[0] = new Dog("chihuawa","white", 4 , false, false, null);
+		
 		
 
 	}
